e4d6c876a6d4e8f50850e463caed25c29153229a
Daisy Jimenez <jimenez.daisy9494@gmail.com>
2025-01-08 16:29:31 -0800

    functions con. lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(defaultActivity = 'roller-skate') { 
+   return `This Saturday, I want to ${defaultActivity}!`; 
+}
+saturdayFun('bathe my dog');
+
+function mondayWork(defaultActivity = 'go to the office') {
+   return `This Monday, I will ${defaultActivity}.`; 
+}
+
+function wrapAdjective(defaultParameter = '*a hard worker*') {
+    return function(adjective = 'special') {
+        return `You are ${defaultParameter}${adjective}${defaultParameter}!`;
+    }
+}
